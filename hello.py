59e1c2e8f14004ab1518cad27962f02cce8db0ed
rawiwan <rawiwan.jwk@gmail.com>
2026-01-14 11:42:29 +0000

    Add new function

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my first script
 print("hello")
 print("I am using github with posit.cloud")
+
+def hello():
+  name = input("What's your name: ")
+  print(f"Hello {name}!")
+  
+# test function
+hello()
